980b9900d54584bdf6f5df1ccc0af0631d8aca8c
Administrator <sardar@mypaperlessoffice.com>
2020-01-04 00:46:28 +0500

    Add textAppearance to replace style

--- andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.gdgfinder.R
 
@@ -22,7 +23,17 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
 
-        return view
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+            Observer<Boolean> { shouldNavigate ->
+                if (shouldNavigate == true) {
+                    val navController = binding.root.findNavController()
+                    navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                    viewModel.onNavigatedToSearch()
+                }
+            })
+
+        return binding.root
     }
 }
