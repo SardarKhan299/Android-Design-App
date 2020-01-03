ceb5ec4341f065483017ae32f3717a1e134dbec5
Administrator <sardar@mypaperlessoffice.com>
2020-01-04 01:28:08 +0500

    Add support for RTL languages.

--- andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
 
 class HomeFragment : Fragment() {
@@ -23,17 +24,16 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        binding.viewModel = viewModel
 
         viewModel.navigateToSearch.observe(viewLifecycleOwner,
             Observer<Boolean> { shouldNavigate ->
                 if (shouldNavigate == true) {
-                    val navController = binding.root.findNavController()
+                    val navController = this.findNavController()
                     navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
                     viewModel.onNavigatedToSearch()
                 }
             })
 
-        return binding.root
+        return view
     }
 }
